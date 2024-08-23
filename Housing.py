30ff9393c8efa99b05da3cfebf054b9046f1b834
Abhinav-Baranwal <abhinavbaranwal17june@gmail.com>
2024-08-23 20:07:52 +0530

    intial commit

---
+++ Housing.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+filepath = r"./HousePricePrediction.xlsx"
+dataset = pd.read_excel(filepath)
+print(dataset.head(5))
+
+print(dataset.shape)
+
+obj = (dataset.dtypes == 'object')
+object_cols = list(obj[obj].index)
+print("Categorical variables:",len(object_cols))
+
+int_ = (dataset.dtypes == 'int')
+num_cols = list(int_[int_].index)
+print("Integer variables:",len(num_cols))
+
+fl = (dataset.dtypes == 'float')
+fl_cols = list(fl[fl].index)
+print("Float variables:",len(fl_cols))
+
+numeric_dataset = dataset.select_dtypes(include=['number'])
+
+correlation_matrix = numeric_dataset.corr()
+
+plt.figure(figsize=(12, 6))
+sns.heatmap(correlation_matrix, cmap='BrBG',fmt='.2f',linewidths=2,annot=True)
+
+plt.title('Correlation Heatmap')
+plt.show()
+
+unique_values = []
+for col in object_cols:
+ unique_values.append(dataset[col].unique().size)
+plt.figure(figsize=(10,6))
+plt.title('No. Unique values of Categorical Features')
+plt.xticks(rotation=90)
+sns.barplot(x=object_cols,y=unique_values)
+plt.xlabel('Categorical Features')
+plt.ylabel('Number of Unique Values')
+plt.show()
+
+plt.figure(figsize=(18, 36))
+plt.title('Categorical Features: Distribution')
+plt.xticks(rotation=90)
+index = 1
+
+for col in object_cols:
+	y = dataset[col].value_counts()
+	plt.subplot(11, 4, index)
+	plt.xticks(rotation=90)
+	sns.barplot(x=list(y.index), y=y)
+	index += 1
+plt.show()
+
+#Data_Cleaning:
+dataset.drop(['Id'], axis=1, inplace=True)
+
+dataset['SalePrice'] = dataset['SalePrice'].fillna(dataset['SalePrice'].mean())
+
+new_dataset = dataset.dropna()
+new_dataset.isnull().sum()
+print(new_dataset)
+
+
